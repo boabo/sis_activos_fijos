78243dff67ea806cccdc4e21cdd5259465c21904
dora <dcastro@boa.bo>
2014-03-17 09:28:45 -0400

    patch de activos fijos

---
+++ base/data000001.sql
@@ -0,0 +1,19 @@
+
+/***********************************I-DAT-RAC-AF-0-12/03/2014*****************************************/
+
+
+INSERT INTO segu.tsubsistema ( codigo, nombre, fecha_reg, prefijo, estado_reg, nombre_carpeta, id_subsis_orig)
+VALUES ('AF', 'Activos Fijos', '2013-02-06', 'AF', 'activo', 'activos_fijos', NULL);
+
+--
+
+----------------------------------
+--COPY LINES TO data.sql FILE  
+---------------------------------
+
+select pxp.f_insert_tgui ('SISTEMA DE ACTIVOS FIJOS', '', 'AF', 'si', , '', 1, '', '', 'AF');
+select pxp.f_insert_tgui ('Preingreso', 'Preingreso de Activos Fijos', 'PREIACT', 'si', 1, 'sis_almacenes/vista/preingreso/PreingresoAct.php', 2, '', 'PreingresoAct', 'AF');
+select pxp.f_insert_testructura_gui ('AF', 'SISTEMA');
+select pxp.f_insert_testructura_gui ('PREIACT', 'AF');
+
+/***********************************F-DAT-RAC-AF-0-12/03/2014*****************************************/
